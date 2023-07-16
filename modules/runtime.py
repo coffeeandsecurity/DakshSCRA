502c19c72f90179d6ca0224bc7c9792e9443362a
Coffee & Security <104401469+coffeeandsecurity@users.noreply.github.com>
2023-07-16 21:13:20 +0530

    Release number update

--- modules/runtime.py
+++ modules/runtime.py
@@ -100,7 +100,7 @@ htmltemplates_dir  = Path(str(root_dir) + "/resources/templates/")
 
 author = '''
 =============================================================
-Daksh SCRA (Source Code Review Assist) - Beta Release v0.3
+Daksh SCRA (Source Code Review Assist) - Beta Release v0.4
 
 Author:     Debasis Mohanty 
             www.coffeeandsecurity.com
