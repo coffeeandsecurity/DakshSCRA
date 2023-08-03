0002db30ee6069d934d692ee3235fa0581e7d103
Coffee & Security <104401469+coffeeandsecurity@users.noreply.github.com>
2023-08-04 03:18:28 +0530

    Update runtime.py

--- modules/runtime.py
+++ modules/runtime.py
@@ -101,7 +101,7 @@ estimation_Fpath = Path(str(root_dir) + "/reports/html/estimation.html")
 
 author = '''
 =============================================================
-Daksh SCRA (Source Code Review Assist) - Beta Release v0.4
+Daksh SCRA (Source Code Review Assist) - Beta Release v0.6
 
 Author:     Debasis Mohanty 
             www.coffeeandsecurity.com
@@ -119,7 +119,7 @@ def print_banner():
                '''
     banner = '''
 =============================================================
-Daksh SCRA (Source Code Review Assist) - Beta Release v0.2
+Daksh SCRA (Source Code Review Assist)
 
 Author:     Debasis Mohanty
             www.coffeeandsecurity.com
