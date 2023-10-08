74c042e7f258309634958db3a11b6f6e9f7e5d1a
Coffee & Security <104401469+coffeeandsecurity@users.noreply.github.com>
2023-10-08 13:00:04 +0530

    Fixed few issues related to common rules
    Fixed few issues related to common rules

--- modules/runtime.py
+++ modules/runtime.py
@@ -107,7 +107,7 @@ estimation_Fpath = Path(str(root_dir) + "/reports/html/estimation.html")
 
 author = '''
 =============================================================
-Daksh SCRA (Source Code Review Assist) - Beta Release v0.11
+Daksh SCRA (Source Code Review Assist) - Beta Release v0.12
 
 Author:     Debasis Mohanty 
             www.coffeeandsecurity.com
